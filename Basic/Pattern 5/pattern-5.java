e6d3a10200b4ace355d3eff4a4633f1fb9453bad
Vishal Prakash <85199417+prakashvishal93@users.noreply.github.com>
2023-07-06 09:51:12 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Pattern 5/pattern-5.java
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj=new Solution();
+            obj.printTriangle(n);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        for(int row = 1 ; row <= n; row++){
+            for(int col = 1; col <= n - row + 1; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
