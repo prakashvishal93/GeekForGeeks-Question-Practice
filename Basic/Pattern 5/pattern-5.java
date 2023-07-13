ea208eda58a61ba25f1e2579c95d3e79146b30bf
Vishal Prakash <85199417+prakashvishal93@users.noreply.github.com>
2023-07-13 20:42:52 +0530

    Updated Solution - GfG to GitHub

--- Basic/Pattern 5/pattern-5.java
+++ Basic/Pattern 5/pattern-5.java
@@ -21,9 +21,10 @@ class Solution {
 
     void printTriangle(int n) {
         // code here
-        for(int row = 1 ; row <= n; row++){
-            for(int col = 1; col <= n - row + 1; col++){
+        for(int row = 1; row <= n; row++){
+            for(int col = 0; col < n - row + 1 ; col++){
                 System.out.print("* ");
+                
             }
             System.out.println();
         }
