10706cc1b1caf95a780faf6ccc7ddbfd42760347
Vishal Prakash <85199417+prakashvishal93@users.noreply.github.com>
2023-07-13 20:24:21 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Pattern 1/pattern-1.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            obj.printSquare(n);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    void printSquare(int n) {
+        
+        
+        // code here
+        
+        for(int row = 0; row < n; row++){
+            for(int col = 0; col < n; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
