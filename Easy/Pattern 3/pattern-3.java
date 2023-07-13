0eda3bb13ac1e22d808cd8ff45998fceaf93993b
Vishal Prakash <85199417+prakashvishal93@users.noreply.github.com>
2023-07-13 20:35:15 +0530

    Updated Solution - GfG to GitHub

--- Easy/Pattern 3/pattern-3.java
+++ Easy/Pattern 3/pattern-3.java
@@ -23,7 +23,7 @@ class Solution {
     void printTriangle(int n) {
         // code here
         for(int row = 1; row <= n; row++){
-            for(int col = 1; col <= row ; col++){
+            for(int col = 1; col <= row; col++){
                 System.out.print(col + " ");
             }
             System.out.println();
